ae2fcec02798bbf046938baf58133363afd5fd25
alexandrev-tibco <alexandre.vazquez@cloud.com>
2025-07-09 20:30:50 +0200

    Initial version 1.8.0 v0

--- packages/app/src/translations/catalogIndex.ts
+++ packages/app/src/translations/catalogIndex.ts
@@ -4,6 +4,7 @@
 
 import { createTranslationResource } from '@backstage/core-plugin-api/alpha';
 import { catalogTranslationRef } from '@backstage/plugin-catalog/alpha';
+import { scaffolderTranslationRef } from '@backstage/plugin-scaffolder/alpha';
 
 export const catalogMessages = createTranslationResource({
   ref: catalogTranslationRef,
@@ -17,3 +18,18 @@ export const catalogMessages = createTranslationResource({
       }),
   },
 });
+
+export const scaffolderMessages = createTranslationResource({
+  ref: scaffolderTranslationRef,
+  translations: {
+    en: () =>
+      // @ts-ignore
+      Promise.resolve({
+        default: {
+          'fields.gitlabRepoPicker.owner.title': 'Gitlab Subgroup Path',
+        },
+      }),
+  },
+});
+
+  
