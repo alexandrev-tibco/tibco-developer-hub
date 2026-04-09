3b45e4f1a885c63e7fd6332ec6f929172d49114f
alexandrev-tibco <alexandre.vazquez@cloud.com>
2026-04-09 15:05:14 +0200

    initial version

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -3,8 +3,11 @@
  */
 
 import { createBackend } from '@backstage/backend-defaults';
+import { scaffolderActionsExtensionPoint } from '@backstage/plugin-scaffolder-node/alpha';
+import { createBackendModule } from '@backstage/backend-plugin-api';
 import 'global-agent/bootstrap';
 import { setGlobalDispatcher, EnvHttpProxyAgent } from 'undici';
+import { executeShellCommandAction } from '@internal/plugin-scaffolder-backend-module-execute-shell';
 
 setGlobalDispatcher(new EnvHttpProxyAgent());
 
@@ -22,9 +25,26 @@ backend.add(
 backend.add(
   import('@internal/plugin-scaffolder-backend-module-tibco-git-repositories'),
 );
+
 backend.add(import('@backstage/plugin-scaffolder-backend'));
 backend.add(import('@backstage/plugin-scaffolder-backend-module-github'));
 backend.add(import('@backstage/plugin-scaffolder-backend-module-gitlab'));
+
+const scaffolderModuleCustomExtensions = createBackendModule({
+  pluginId: 'scaffolder', // name of the plugin that the module is targeting
+  moduleId: 'custom-extensions',
+  register(env) {
+    env.registerInit({
+      deps: {
+        scaffolder: scaffolderActionsExtensionPoint
+      },
+      async init({ scaffolder }) {
+        scaffolder.addActions(new (executeShellCommandAction as  any)());
+      },
+    });
+  },
+});
+backend.add(scaffolderModuleCustomExtensions);
 backend.add(import('@backstage/plugin-techdocs-backend'));
 
 // auth plugin
@@ -32,6 +52,7 @@ backend.add(import('@backstage/plugin-auth-backend'));
 backend.add(import('./authModuleOidcProvider.ts'));
 backend.add(import('@backstage/plugin-auth-backend-module-github-provider'));
 backend.add(import('@backstage/plugin-auth-backend-module-guest-provider'));
+backend.add(import('./authModuleGitlabCookieProvider.ts'));
 
 // catalog plugin
 backend.add(import('@backstage/plugin-catalog-backend'));
@@ -59,6 +80,7 @@ backend.add(import('@backstage/plugin-search-backend-module-techdocs'));
 // kubernetes
 backend.add(import('@backstage/plugin-kubernetes-backend'));
 backend.add(import('@internal/plugin-scaffolder-backend-module-metrics-api'));
+backend.add(import('@internal/plugin-scaffolder-backend-module-trigger-jenkins-job'));
 backend.add(import('./addEssentialLocation'));
 backend.add(import('./cachePlugin.ts'));
 
